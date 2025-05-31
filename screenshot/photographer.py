055af69664b2ce7b799d49dae173625c5f463ca2
Alessandro Calista <alessandromaria.calista@gmail.com>
2025-05-31 19:37:02 +0200

    Added preconditions checks inside screenshot function

--- screenshot/photographer.py
+++ screenshot/photographer.py
@@ -6,11 +6,11 @@ import logging
 SCREENSHOT_NAME_FORMAT = "screenshot_$url$.jpg"
 logging.basicConfig(level=logging.ERROR)
 
-def safe_url(url: str) -> str:
+def netloc_from_url(url: str) -> str:
     parsed = urlparse(url)
     return parsed.netloc
 
-def is_reachable(url: str) -> bool:
+def check_reachable(url: str) -> bool:
     try:
         response = requests.head(url, timeout=5)
         return True
@@ -18,26 +18,32 @@ def is_reachable(url: str) -> bool:
         return False
 
 def is_valid_and_reachable(url: str) -> bool:
+    if url is None or len(url) == 0:
+        return False
+
     parsed = urlparse(url)
     if not parsed.scheme and not parsed.netloc:
         return False
-    return is_reachable(url)
+    return check_reachable(url)
 
-def screenshot(page_link: str, output_folder: str) -> str:
+def screenshot(page_link: str, output_folder: str) -> tuple[bool,str,str]:
     with sync_playwright() as p:
+        if not is_valid_and_reachable(page_link):
+            return (False, "Invalid URL", "")
+
         browser = p.chromium.launch()
         context = browser.new_context()
         page = context.new_page()
-
-        output = output_folder + "/" + SCREENSHOT_NAME_FORMAT.replace('$url$', safe_url(page_link))
+        output = output_folder + "/" + SCREENSHOT_NAME_FORMAT.replace('$url$', netloc_from_url(page_link))
 
         try:
-            page.goto(url=page_link, wait_until="domcontentloaded", referer="alessandrocalista.it")
+            page.goto(url=page_link, wait_until="domcontentloaded", referer=page_link)
             page.screenshot(type="jpeg", path=output, full_page=True)
         except TimeoutError as _:
             logging.error("La pagina ha impiegato troppo tempo per rispondere", exc_info=True)
+            return (False, "Timed Out", "")
 
         page.close()
         context.close()
         browser.close()
-        return output
+        return (True, "URL Valido", output)
