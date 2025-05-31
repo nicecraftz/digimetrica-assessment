bb75f4b945f02dbe2e01311430afeaff190fdac4
Alessandro Calista <alessandromaria.calista@gmail.com>
2025-05-31 16:09:51 +0200

    Minor changes

--- visualization/streamlit_app.py
+++ visualization/streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
+from screenshot import photographer
 
+RESOURCES_FOLDER = "output"
 
-def get_screenshot(url: str):
-    print("Hello World")
+def get_screenshot():
+    input_url = st.session_state.input_url
+    if (input_url is None or len(input_url) == 0) or not photographer.is_valid_and_reachable(input_url):
+        st.write("URL non valido!")
+        return
 
-input = st.text_input(label="Inserisci URL")
-st.button(label="Elabora",icon="🧑🏻‍💻", on_click=get_screenshot(input), type="primary")
+    path = photographer.screenshot(input_url, output_folder=RESOURCES_FOLDER)
+    st.image(path, use_container_width=True)
+
+    with open(path, "rb") as screenshot:
+        st.download_button(
+            label="Scarica immagine",
+            icon="📥",
+            data=screenshot,
+            file_name="screenshot.jpg",
+            mime="image/jpeg"
+        )
+
+st.text_input(label="Inserisci URL", key="input_url")
+st.button(label="Elabora", icon="🧑🏻‍💻", on_click=get_screenshot, type="primary")
