0bc388d768798cb3dc2f5af5db08fc597e7a3559
Alessandro Calista <alessandromaria.calista@gmail.com>
2025-05-31 19:38:09 +0200

    Removed url safeguard since it was added to photographer module.

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def main():
         urls = set(file_data["urls"])
 
     for url in urls:
-        if photographer.is_valid_and_reachable(url):
-            photographer.screenshot(url, output_folder=RESOURCES_FOLDER)
+        photographer.screenshot(url, output_folder=RESOURCES_FOLDER)
 
 if __name__ == "__main__":
     main()
