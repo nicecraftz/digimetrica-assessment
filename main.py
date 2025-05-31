c73b28d507a98f2a6dc9194bcd4e2282ef486a5f
Alessandro Calista <alessandromaria.calista@gmail.com>
2025-05-31 12:30:31 +0200

    Minor changes to main to include the external screenshot functionality

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-import requests
-import urllib.parse
-from playwright.sync_api import Page, sync_playwright
-import streamlit
+from screenshot import photographer
 import json
 
 DATA = "data.json"
 RESOURCES_FOLDER = "output"
-SCREENSHOT_NAME_FORMAT = "screenshot_$url$.jpg"
-
-def safe_url(url: str) -> str:
-    parsed = urllib.parse.urlparse(url)
-    return parsed.netloc
-
-def screenshot_page(page: Page, page_link: str):
-    page.goto(url=page_link, wait_until="domcontentloaded", referer="alessandrocalista.it")
-    page.screenshot(type="jpeg", path=f"{RESOURCES_FOLDER}/{SCREENSHOT_NAME_FORMAT.replace('$url$', safe_url(page_link))}", full_page=True)
 
 def main():
     urls = []
@@ -22,15 +10,8 @@ def main():
         file_data = json.load(file)
         urls = set(file_data["urls"])
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        context = browser.new_context()
-        page = context.new_page()
-        for url in urls:
-            screenshot_page(page, url)
-        page.close()
-        context.close()
-        browser.close()
+    for url in urls:
+        photographer.screenshot(url, output_folder=RESOURCES_FOLDER)
 
 if __name__ == "__main__":
     main()
